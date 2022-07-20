471ccff8ad380e7d49bd3d5c617439870ef2ac76
Incperktor <sas270694@yandex.ru>
2022-07-20 15:53:32 +0300

    SQL

---
+++ INSERT.sql
@@ -0,0 +1,26 @@
+INSERT INTO genres (genre_name)
+VALUES ('Rock'), ('Electro'), ('Hip hop'), ('Chanson'), ('Classical music'), ('Metal');
+
+INSERT INTO artists (artist_name)
+VALUES ('Slipknot'), ('Nirvana'),
+('The Prodigy'), ('Gorillaz'),
+('Eminem'), ('Snoop Dogg'),
+('Zaz'), ('Carla Bruni'),
+('Johannes Brahms'), ('Franz Schubert'),
+('Metallica'), ('System of a Down');
+
+INSERT INTO genres_artists (genres_id, artists_id)
+VALUES (1,1), (1,2),
+(2,3), (2,4),
+(3,5), (3,6),
+(4,7), (4,8),
+(5,9), (5,10),
+(6,11), (6,12),
+(2,9);
+
+INSERT INTO albums (album_name, album_date)
+VALUES ('The Gray Chapter'), ('All Hope Is Gone'),
+('Nevermind'), ('Incesticide'),
+('')
+
+
